dd2fd7f062ec84bdc0097898a9c1144816d11813
BlackAsu <44605334+Z01Ntasu@users.noreply.github.com>
2019-01-12 00:41:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,18 +11,23 @@ wks = gc.open('person Database').sheet1
 run = True
 while run:
 	option = input("1.Person erstellen \n2.Person suchen\n3.Programm verlassen\n")
+	print("-----------------------------------------")
 	if  option == "1":
+		
 		cell = wks.find("next")
 		wks.update_cell(cell.row, cell.col,input("Name?\n") )
 		wks.update_cell(cell.row+1, cell.col, input("Nachname?\n"))
 		wks.update_cell(cell.row+2, cell.col, input("Alter?\n"))
 		wks.update_cell(cell.row+3, cell.col, input("Beruf?\n"))
 		wks.update_cell(cell.row, cell.col+1, 'next')
+		print("-----------------------------------------")
 	elif option == "2":
-		cell = wks.find(input("Gebe den Vor oder nachnamen ein\n"))
+		cell = wks.find(input("Gebe den Vor oder Nachnamen ein!\n"))
+		print("-----------------------------------------")
 		print("Vorname : ",cell.value)
 		print("Nachname : ",wks.cell(cell.row+1, cell.col).value)
 		print("Alter : ",wks.cell(cell.row+2, cell.col).value)
 		print("Beruf : ",wks.cell(cell.row+3, cell.col).value)
+		print("-----------------------------------------")
 	else:
 		run = False
